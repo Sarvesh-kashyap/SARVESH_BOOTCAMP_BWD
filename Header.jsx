dd6c2d8a12499712655893714f14100449126aa1
Sarvesh-kashyap <85389489+Sarvesh-kashyap@users.noreply.github.com>
2021-06-06 18:32:44 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+  <header>
+    <h1><b><i>About ShapeAI</i></b></h1>
+  </header>
+  ); 
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Note() {
+  return (
+  <div className="note">
+    <h2><u>JavaScreipt and React.js</u></h2> 
+
+    
+    <h1><p><b><i> We at ShapeAI (A research and education-based startup )
+       are a team that plans to give students a chance to perform
+        research and make projects in the areas of AI, ML, 
+        Web design Development. We aim to provide an interactive
+         learning platform to students where they will be able to
+          work with us and develop projects and write blogs on the 
+          topics of ML, AI, DL and web designing and development.
+          </i></b> </p></h1>
+  </div>
+     
+  ); 
+}
+
+export default Note; 
